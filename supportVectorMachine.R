b34bfaac183f7d0be9f8106723ddba78f62673b5
bertauchekurtis <kurtisbertauche@gmail.com>
2022-09-26 10:28:21 -0700

    svm work

--- supportVectorMachine.R
+++ supportVectorMachine.R
@@ -4,70 +4,41 @@
 
 # libraries
 library(e1071)
+library(caret)
 set.seed(37)
 
 # load data
 train_data <- read.csv("data/clean/training.csv")
 test_data <- read.csv("data/clean/testing.csv")
 
-costs = c(0.01, 0.05, 0.1, 0.5, 1, 2, 5, 10, 20, 50)
-
 svm_model <- NULL
-accuracy <- 0
 
-for(cost in costs)
-{
-  train_control <- trainControl(method = "cv", number = 5)
-  set.seed(37)
-  svm_model_tune <- train(factor(Outcome) ~ .,
-                          data = train_data,
-                          method = "svmLinear",
-                          type = "C-svc",
-                          trControl = train_control,
-                          tuneGrid = data.frame(C=c(cost)))
-  
-  if(svm_model_tune$results$Accuracy > accuracy)
-  {
-    accuracy <- svm_model_tune$results$Accuracy
-    svm_model <- svm_model_tune
-    save(svm_model, file = "models/svm_model.RData")
-    print("New Best Model! Accuracy:")
-    print(accuracy)
-    print("Cost:")
-    print(cost)
-  }
-  else
-  {
-    print("Model Finished - no improvement - Cost:")
-    print(cost)
-  }
-}
+train_control <- trainControl(method = "cv", number = 5)
 
-for(cost in costs)
-{
-  train_control <- trainControl(method = "cv", number = 5)
-  set.seed(37)
-  svm_model_tune <- train(factor(Outcome) ~ .,
-                          data = train_data,
-                          method = "svmRadial",
-                          type = "C-svc",
-                          trControl = train_control,
-                          tuneGrid = data.frame(C=c(cost)))
-  
-  if(svm_model_tune$results$Accuracy > accuracy)
-  {
-    accuracy <- svm_model_tune$results$Accuracy
-    svm_model <- svm_model_tune
-    save(svm_model, file = "models/svm_model.RData")
-    print("New Best Model! Accuracy:")
-    print(accuracy)
-    print("Cost:")
-    print(cost)
-  }
-  else
-  {
-    print("Model Finished - no improvement - Cost:")
-    print(cost)
-  }
-}
+# LINEAR SVM
+linear_svm <- train(as.factor(Outcome) ~.,
+                    data = train_data,
+                    method = "svmLinear",
+                    trControl = train_control,
+                    tuneGrid = expand.grid(C = seq(0.01, 2, length = 20)))
+plot(linear_svm)
 
+best_linear_svm <- svm(as.factor(Outcome) ~ .,
+                         data = train_data,
+                         kernel = "linear",
+                         cost = linear_svm$bestTune)
+
+svm_model <- best_linear_svm
+
+# RADIAL KERNEL
+raidal_svm <- train(as.factor(Outcome) ~.,
+                    data = train_data,
+                    method = "svmRadial",
+                    trControl = train_control,
+                    tuneLength = 20)
+
+best_radial_svm <- svm(as.factor(Outcome) ~ .,
+                         data = train_data,
+                         kernel = "radial",
+                         cost = raidal_svm$bestTune[2],
+                         sigma = raidal_svm$bestTune[1])
